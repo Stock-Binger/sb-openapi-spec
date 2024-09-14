7329a1565356815051ade4d370917298e922495e
GouthamAkula <akulagoutham23@gmail.com>
2024-09-14 18:59:02 +0530

    update spec file

--- merge-specs.sh
+++ merge-specs.sh
@@ -7,7 +7,7 @@ SPEC_DIR=$(pwd)
 OUTPUT_FILE="$SPEC_DIR/sb-openapi-spec.yml"
 
 # Find all YAML files starting with sb-openapi*.yml (including sb-openapi-spec.yml)
-OPENAPI_FILES=$(ls $SPEC_DIR/sb-openapi*.yml)
+OPENAPI_FILES=$(ls $SPEC_DIR/sb-openapi*.yaml)
 
 # Merge all files into the main sb-openapi-spec.yml
 redocly join $OPENAPI_FILES -o $OUTPUT_FILE
